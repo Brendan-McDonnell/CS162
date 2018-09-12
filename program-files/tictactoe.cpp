58bd438cd9e6fcebdacfe1ff7ac836dd66646bcd
Brendan McDonnell <brendanm149@gmail.com>
2018-09-12 06:06:06 +0100

    Built main board and visualiser

--- program-files/tictactoe.cpp
+++ program-files/tictactoe.cpp
@@ -1,36 +1,55 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-const int PLAY_EMPTY = 0;
-const int PLAY_X = 1;
-const int PLAY_O = 2;
-
 // Prototypes
-
+void printBoard(char** b);
 
 // Main method
 int main() {
 
-  int** ptrBoard = new int*[3];
+  char** ptrBoard = new char*[3];
   for (int i = 0; i < 3; i++) {
-    ptrBoard[i] = new int[3];
+    ptrBoard[i] = new char[3];
   }
-  
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      ptrBoard[i][j] = 0;
+      ptrBoard[i][j] = '.';
     }
   }
-  
-  
-  
+
+  printBoard(ptrBoard);
+
   return 0;
 }
 
 // Methods
 
-void printBoard(int** b) {
-  system("CLS");
-  char output[24];
-  // OUTPUT
+void printBoard(char** b) {
+  cout << endl << endl;
+  char output[22];
+  memset(output, 0x00, 34);
+  
+  int index = 0;
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      if (col == 0) {
+	output[index++] = b[row][col];
+	output[index++] = '\t';
+      }
+      else if (col == 1) {
+	output[index++] = b[row][col];
+	output[index++] = '\t';
+      }
+      else if (col == 2) {
+	output[index++] = b[row][col];
+	output[index++] = '\n';
+	output[index++] = '\n';
+      }
+    }
+  }
+  
+  cout << output << endl;
+}
